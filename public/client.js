ca305c0b85096910df17eadc47e8a789e224c250
Glitch (hello-express) <none>
2019-04-20 14:04:06 +0000

    🕌📺 Checkpoint ./public/client.js:4005004/987

--- public/client.js
+++ public/client.js
@@ -7,7 +7,7 @@ ws= new WebSocket(proto_ws+"://"+window.location.hostname);
 ws.onmessage= m => console.log("WS LLEGO",m) 
 
 ws.onopen= () => {
-ws.send("Hola!")
+ws.send(JSON.stringify({m: "anonimo", texto: "hola!"}));
 console.log("WS listo, envia con ws.send('mi mensaje')");
 }
 
@@ -17,11 +17,12 @@ function enviar(msg) {
 //========================================================
 const { Component, h, render } = window.preact;
 
-Mensajes= (props) =>
+Mensajes= (props,state) =>
   h('div', {},
         props.mensajes.map(m => 
           h('div', {},
-            h('span',{},m.de),
+            h('span',{style: "display: inline-block; width: 15em"},m.de),
+            h(':'),
             h('span',{},m.texto)
           )
         )
@@ -31,25 +32,37 @@ class Chat extends Component {
   state= {
     mensajes: [],
   }
+  apodo_el= null; //U: el elemento donde esta el apodo
+  mensaje_el= null; //U: el elemento donde está el mensaje
 
   componentDidMount() {
     ws.onmessage= m => {
       console.log("WS LLEGO",m);
-      this.setState({ mensajes: this.state.mensajes.push(JSON.parse(m)) });
+      this.state.mensajes.push(JSON.parse(m.data))
+      this.setState({ mensajes: this.state.mensajes });
     }
   }
   //A: cuando recibimos un mensaje lo agregamos a la lista, y asi se redibuja la UI
   
+  enviar() {
+    var apodo= this.apodo_el.value || 'anonimo';
+    var msj= this.mensaje_el.value;
+    if (!msj) { alert("Escribi un mensaje!") }
+    else {
+      ws.send(JSON.stringify({de: apodo, texto: msj}));
+    }
+  }
+
   render(props, state) {
     return h('div',{},
         h('div',{},
           h('span',{},"Apodo:"),
-          h('input',{onChange: e => console.log("E",window.x=e) }),
+          h('input',{ ref: e => (this.apodo_el=e) }),
         ),
         h(Mensajes, {mensajes: state.mensajes}),
         h('div',{},
           h('span',{},"Mensaje:"),
-          h('input',{onChange: e => console.log("E",window.x=e) }),
+          h('input',{ref: e => (this.mensaje_el=e)}),
         ),
         h('div',{},
           h('button',{onClick: e => this.enviar()}, "Enviar")
