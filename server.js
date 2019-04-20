83c0d3c363139f19b0fda242535648803eb02c05
Glitch (hello-express) <none>
2019-04-20 12:10:34 +0000

    🏓💓 Checkpoint ./views/index.html:4005004/887 ./public/client.js:4005004/1204 ./package.json:4005004/34 ./server.js:4005004/643

--- server.js
+++ server.js
@@ -20,3 +20,23 @@ app.get('/', function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+var WebSocket= require('ws');
+var wss= new WebSocket.Server({ port: PORT_WS });
+wss.broadcast = function broadcast(data) { //U: Broadcast to all.
+  console.log("MANDO A TODOS ...", data);
+  wss.clients.forEach(function each(client) {
+    if (client.readyState === WebSocket.OPEN) { client.send(data); }
+  });
+};
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) { //U: Broadcast to everyone else.
+    console.log("LLEGO ...", data);
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
