3b1040885545b3b75eb277ff3763d4f9fbe9b72a
Glitch (websocket-chat-example) <none>
2020-06-10 19:33:58 +0000

    💽💈 Checkpoint ./public/index.html:217543/210 ./public/websockets/index.html:217543/114 ./public/websockets/client.js:217543/160

--- public/websockets/client.js
+++ public/websockets/client.js
@@ -19,3 +19,11 @@ ws.onmessage = function(message) {
 setTimeout(() => {
   ws.send(JSON.stringify({de: "Example Name", texto: "test"}));
 }, 1000)
+
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(10, 10, 10, 0, 2 * Math.PI);
+ctx.stroke();
