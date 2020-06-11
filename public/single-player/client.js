7e94340645af123f2ec963cd9b205db1b317e44d
Glitch (websocket-chat-example) <none>
2020-06-11 20:03:34 +0000

    🎇🍲 Checkpoint ./public/single-player/client.js:217543/643 ./public/single-player/index.html:217543/574

--- public/single-player/client.js
+++ public/single-player/client.js
@@ -2,11 +2,24 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var x = 10;
 var y = 10;
+const playerColor = randomColor();
+document.getElementById("player-color").style.backgroundColor = playerColor;
+
+function randomColor() {
+  let color = "#";
+  let values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+  color += values[Math.floor(Math.random() * values.length)];
+  color += values[Math.floor(Math.random() * values.length)];
+  color += values[Math.floor(Math.random() * values.length)];
+  return color;
+}
 
 function gameLoop() {
   ctx.clearRect(0, 0, 200, 200);
   ctx.beginPath();
   ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fillStyle = playerColor;
+  ctx.fill();
   ctx.stroke();
   if (x > 190) {
     x = 190;
