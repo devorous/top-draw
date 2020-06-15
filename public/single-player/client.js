bfcf848c8f1435fe509e0341fdc9dde6253392d6
Glitch (websocket-chat-example) <none>
2020-06-15 15:59:40 +0000

    🎒🌾 Checkpoint ./public/single-player/client.js:217543/1022 ./public/websockets/client.js:217543/1387

--- public/single-player/client.js
+++ public/single-player/client.js
@@ -1,8 +1,17 @@
+// Simple game:
+
+// Set up canvas
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
-var x = 10;
-var y = 10;
+
+// Generate random color for player color, also used for unique id
 const playerColor = randomColor();
+
+// Players location storage, add yourself to start:
+var players = [
+  {x: 10, y: 10, color: playerColor} 
+]
+
 document.getElementById("player-color").style.backgroundColor = playerColor;
 
 function randomColor() {
@@ -14,46 +23,48 @@ function randomColor() {
   return color;
 }
 
-function gameLoop() {
+function gameLoop(player) {
   ctx.clearRect(0, 0, 200, 200);
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.arc(player.x, player.y, 10, 0, 2 * Math.PI);
   ctx.fillStyle = playerColor;
   ctx.fill();
   ctx.stroke();
-  if (x > 190) {
-    x = 190;
+  
+  // Detect player going past boundries
+  if (player.x > 190) {
+    player.x = 190;
   }
-  if (x < 10) {
-    x = 10;
+  if (player.x < 10) {
+    player.x = 10;
   }
-  if (y > 190) {
-    y = 190;
+  if (player.y > 190) {
+    player.y = 190;
   }
-  if (y < 10) {
-    y = 10;
+  if (player.y < 10) {
+    player.y = 10;
   }
-  setTimeout(gameLoop, 1000/30);
 }
 
-gameLoop();
+// Start game loop, run 30 times per second
+setInterval(gameLoop.bind(this, players[0]), 1000/30);
 
-document.addEventListener('keydown', logKey);
+document.addEventListener('keydown', detectKeyPress.bind(this, players[0]));
 
-function logKey(e) {
+function detectKeyPress(e, player) {
   const speed = 5;
   switch(e.code) {
     case "ArrowUp":
-      y -= speed;
+      player.y -= speed;
       break;
     case "ArrowLeft":
-      x -= speed;
+      player.x -= speed;
       break;
     case "ArrowDown":
-      y += speed;
+      player.y += speed;
       break;
     case "ArrowRight":
-      x += speed;
+      player.x += speed;
       break;
   }
 }
--- public/websockets/client.js
+++ public/websockets/client.js
@@ -1,5 +1,3 @@
-var messages = [];
-
 // Match websocket protocol to page protocol (ws/http or wss/https):
 var wsProtocol= window.location.protocol=="https:" ? "wss" : "ws"; 
 
@@ -9,6 +7,7 @@ connection.onopen = function() {
   console.log("Websocket connected!");
 }
 
+var messages = [];
 connection.onmessage = function(message) {
   console.log("New Message:");
   console.log(message);
@@ -19,3 +18,67 @@ connection.onmessage = function(message) {
 setTimeout(() => {
   connection.send(JSON.stringify({de: "Example Name", texto: "test"}));
 }, 1000)
+
+// Simple game:
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var x = 10;
+var y = 10;
+const playerColor = randomColor();
+document.getElementById("player-color").style.backgroundColor = playerColor;
+
+function randomColor() {
+  let color = "#";
+  let values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
+  color += values[Math.floor(Math.random() * values.length)];
+  color += values[Math.floor(Math.random() * values.length)];
+  color += values[Math.floor(Math.random() * values.length)];
+  return color;
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, 200, 200);
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fillStyle = playerColor;
+  ctx.fill();
+  ctx.stroke();
+  
+  // Detect player going past boundries
+  if (x > 190) {
+    x = 190;
+  }
+  if (x < 10) {
+    x = 10;
+  }
+  if (y > 190) {
+    y = 190;
+  }
+  if (y < 10) {
+    y = 10;
+  }
+}
+
+// Start game loop, run 30 times per second
+setInterval(gameLoop, 1000/30);
+
+document.addEventListener('keydown', detectKeyPress);
+
+function detectKeyPress(e) {
+  const speed = 5;
+  switch(e.code) {
+    case "ArrowUp":
+      y -= speed;
+      break;
+    case "ArrowLeft":
+      x -= speed;
+      break;
+    case "ArrowDown":
+      y += speed;
+      break;
+    case "ArrowRight":
+      x += speed;
+      break;
+  }
+}
