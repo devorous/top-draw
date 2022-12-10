8442c2a111d4ec3e8f2c6821889983ce45156ff0
Glitch (multiplayer-websockets-example) <none>
2022-12-10 09:51:11 +0000

    🍮🍐 Checkpoint ./.glitch-assets:3497662/215 ./public/client.js:3497662/22524 ./server.js:3497662/6746

--- public/client.js
+++ public/client.js
@@ -1,5 +1,3 @@
-
-
 var users = [];
 
 var userID = Math.floor(Math.random() * 999999);
@@ -14,29 +12,26 @@ var width = board.width;
 var size = 10;
 
 var ctx = board.getContext("2d");
-ctx.imageSmoothingQuality ="high";
+ctx.imageSmoothingQuality = "high";
 
 var current_line = [];
 
 
-var test = "test";
-
 var self = {
   x: 0,
   y: 0,
-  lastx:null,
-  lasty:null,
-  size:10,
+  lastx: null,
+  lasty: null,
+  size: 10,
   color: "black",
-  tool:"brush",
-  text:"",
+  tool: "brush",
+  text: "",
   mousedown: false,
-  id:userID
+  id: userID,
 };
 
 // Add self player to beginning of players array:
 users.push(self);
-
 
 /* - - - - - - - - - -
    Setup Websocket:
@@ -46,414 +41,393 @@ users.push(self);
 var wsProtocol = window.location.protocol == "https:" ? "wss" : "ws";
 
 // Set up new websocket connection to server
-var socket = new WebSocket(`${wsProtocol}://${window.location.hostname}:${window.location.port}`);
+var socket = new WebSocket(
+  `${wsProtocol}://${window.location.hostname}:${window.location.port}`
+);
 
 // Log successful connection
-socket.onopen = function() {
-  send({command:"connect",userdata:self,id:userID});
+socket.onopen = function () {
+  send({ command: "connect", userdata: self, id: userID });
   console.log("Websocket connected!");
 };
 
 socket.addEventListener("message", (m) => {
   var data = JSON.parse(m.data);
 
-  switch(data.command){
-    case 'currentUsers':
-        //updating list of users with new users
-        var current_ids = [];
-        for(var i=0;i<users.length;i++){
-          if(current_ids.indexOf(users[i].id)==-1){
-            current_ids.push(users[i].id);
-          }
+  switch (data.command) {
+    case "currentUsers":
+      //updating list of users with new users
+      var current_ids = [];
+      for (var i = 0; i < users.length; i++) {
+        if (current_ids.indexOf(users[i].id) == -1) {
+          current_ids.push(users[i].id);
         }
-        //getting all the current ids to check for duplicates
-        for(var i=0;i<data.users.length;i++){
-          var user = data.users[i]
-          if(current_ids.indexOf(data.users[i].id) == -1 && data.users[i].id != userID){
-              users.push(data.users[i].userdata);
-              drawUser(data.users[i].userdata,data.users[i].id);
-          }
+      }
+      //getting all the current ids to check for duplicates
+      for (var i = 0; i < data.users.length; i++) {
+        var user = data.users[i];
+        if (
+          current_ids.indexOf(data.users[i].id) == -1 &&
+          data.users[i].id != userID
+        ) {
+          users.push(data.users[i].userdata);
+          drawUser(data.users[i].userdata, data.users[i].id);
         }
-        break
-    case 'connect':
-      break
-    case 'userLeft':
+      }
+      break;
+    case "connect":
+      break;
+    case "userLeft":
       //when a user leaves, update the user list and remove the cursor by ID class
-      users = users.filter(userdata =>{
+      users = users.filter((userdata) => {
         return userdata.id != data.id;
       });
-      var div = $("."+data.id.toString())[0];
-      if(div){
+      var div = $("." + data.id.toString())[0];
+      if (div) {
         div.remove();
       }
-      break
-      
-    case 'broadcast':
+      break;
+
+    case "broadcast":
       recieve(data);
   }
 });
 
-
-function send(data){
+function send(data) {
   socket.send(JSON.stringify(data));
 }
 
-function recieve(data){
+function recieve(data) {
   //process broadcast events
   var user = getUser(data.id);
-  switch(data.type){
-      
-    case 'clear':
-        clearBoard();
-      break
-      
-    case 'Mm':
+  switch (data.type) {
+    case "clear":
+      clearBoard();
+      break;
+
+    case "Mm":
       moveCursor(data);
       //if user has no lastpos, make it the current pos
-      if(user.lastx==null){
+      if (user.lastx == null) {
         user.lastx = data.x;
         user.lasty = data.y;
       }
-      updateUser(user, data,['x','y']);
-      var pos = {x:user.x,y:user.y};
-      var lastpos = {x:user.lastx,y:user.lasty};
-      if(user.mousedown && user.tool=="brush"){
-        drawLine(pos,lastpos,user);
+      updateUser(user, data, ["x", "y"]);
+      var pos = { x: user.x, y: user.y };
+      var lastpos = { x: user.lastx, y: user.lasty };
+      if (user.mousedown && user.tool == "brush") {
+        drawLine(pos, lastpos, user);
       }
-      break
-      
-    case 'Md':
-      
-      user.lastx=user.x;
-      user.lasty=user.y;
-      var pos = {x:user.x,y:user.y};
-      if(user.tool=="brush"){
-        ctx.lineCap="round";
-        drawLine(pos,pos,user);
+      break;
+
+    case "Md":
+      user.lastx = user.x;
+      user.lasty = user.y;
+      var pos = { x: user.x, y: user.y };
+      if (user.tool == "brush") {
+        ctx.lineCap = "round";
+        drawLine(pos, pos, user);
       }
-      if(user.tool=="text" && user.text != ""){
-        drawText(user)
-        user.text="";
-        var input = $("."+user.id.toString()+" .textInput")[0];
-        input.innerHTML="";
+      if (user.tool == "text" && user.text != "") {
+        drawText(user);
+        user.text = "";
+        var input = $("." + user.id.toString() + " .textInput")[0];
+        input.innerHTML = "";
       }
-      user.mousedown=true;
-      break
-      
-    case 'Mu':
-      if(user.tool=="brush"){
+      user.mousedown = true;
+      break;
+
+    case "Mu":
+      if (user.tool == "brush") {
         ctx.stroke();
       }
-      user.mousedown=false;
-      break
-      
-    case 'ChS':
+      user.mousedown = false;
+      break;
+
+    case "ChS":
       //change the size
-      updateUser(user, data,['size']);
-      var userText = $("."+user.id.toString()+" .text")[0];
-  
-      userText.style.fontSize=(data.size+5).toString()+"px";
-      break
-      
-    case 'ChT':
+      updateUser(user, data, ["size"]);
+      var userText = $("." + user.id.toString() + " .text")[0];
+
+      userText.style.fontSize = (data.size + 5).toString() + "px";
+      break;
+
+    case "ChT":
       //change the tool
       console.log("changing tool: ");
       console.log(data);
-      updateUser(user,data,['tool']);
-      var userText = $("."+user.id.toString()+" .text")[0];
-      var userCircle = $("."+user.id.toString()+" circle")[0];
-      if(data.tool=="brush"){
-        userText.style.display="none";
-        userCircle.style.display="block";
+      updateUser(user, data, ["tool"]);
+      var userText = $("." + user.id.toString() + " .text")[0];
+      var userCircle = $("." + user.id.toString() + " circle")[0];
+      if (data.tool == "brush") {
+        userText.style.display = "none";
+        userCircle.style.display = "block";
       }
-      if(data.tool=="text"){
-        userText.style.display="block";
-        userCircle.style.display="none";
+      if (data.tool == "text") {
+        userText.style.display = "block";
+        userCircle.style.display = "none";
       }
-      break
-    case 'kp':
+      break;
+    case "kp":
       //keypress
-      if(user.tool=="text"){
-        updateText(data.key,user);
+      if (user.tool == "text") {
+        updateText(data.key, user);
       }
   }
 }
 
-function getUser(id){
-  var user = users.filter( a =>{
-    return a.id==id;
+function getUser(id) {
+  var user = users.filter((a) => {
+    return a.id == id;
   })[0];
-  return user
+  return user;
 }
 
-
-board.addEventListener('mousemove', function(e){
+board.addEventListener("mousemove", function (e) {
   var user = getUser(userID);
-  user.x = e.layerX-100
-  user.y = e.layerY-100
-  
+  user.x = e.layerX - 100;
+  user.y = e.layerY - 100;
+
   //set your cursor pos
-  cursor.style.left=user.x+"px";
-  cursor.style.top=user.y+"px";
+  cursor.style.left = user.x + "px";
+  cursor.style.top = user.y + "px";
 
-  send({command:"broadcast",type:"Mm",x:user.x,y:user.y,id:userID});
-  var lastpos = {x:user.lastx,y:user.lasty};
-  var pos = {x:self.x,y:self.y};
-  if(lastpos.x==null){
-    lastpos=pos;
+  send({ command: "broadcast", type: "Mm", x: user.x, y: user.y, id: userID });
+  var lastpos = { x: user.lastx, y: user.lasty };
+  var pos = { x: self.x, y: self.y };
+  if (lastpos.x == null) {
+    lastpos = pos;
   }
-  if(user.mousedown && user.tool=="brush"){
-    drawLine(pos,lastpos,user);
+  if (user.mousedown && user.tool == "brush") {
+    drawLine(pos, lastpos, user);
   }
-})
+});
 
-board.addEventListener('mousedown', function(e){
+board.addEventListener("mousedown", function (e) {
   var user = getUser(userID);
-  user.lastx=user.x;
-  user.lasty=user.y;
+  user.lastx = user.x;
+  user.lasty = user.y;
   self.mousedown = true;
-  send({command:"broadcast",type:"Md", id:userID})
+  send({ command: "broadcast", type: "Md", id: userID });
 
-  if(user.tool=="brush"){
+  if (user.tool == "brush") {
     ctx.beginPath();
-    ctx.lineCap="round";
-    ctx.lineWidth=user.size*2;
-    ctx.moveTo(e.layerX,e.layerY);
-    ctx.lineTo(e.layerX,e.layerY);
+    ctx.lineCap = "round";
+    ctx.lineWidth = user.size * 2;
+    ctx.moveTo(e.layerX, e.layerY);
+    ctx.lineTo(e.layerX, e.layerY);
     ctx.stroke();
   }
 
-  if(user.tool=="text" && user.text!=""){
+  if (user.tool == "text" && user.text != "") {
     drawText(user);
-    user.text="";
+    user.text = "";
     var input = $(".textInput.self")[0];
-    input.innerHTML="";
+    input.innerHTML = "";
   }
 });
 
-board.addEventListener('mouseup', function(e){
+board.addEventListener("mouseup", function (e) {
   self.mousedown = false;
-  send({command:"broadcast", type:"Mu",id:userID})
-  var line = {path:current_line,id:userID};
-  current_line=[];
+  send({ command: "broadcast", type: "Mu", id: userID });
+  var line = { path: current_line, id: userID };
+  current_line = [];
 });
 
-board.addEventListener('wheel', function(e){
+board.addEventListener("wheel", function (e) {
   var text = $(".text.self")[0];
   e.preventDefault();
-  var step=1;
-  if(size<=1){
-    step=0.1;
+  var step = 1;
+  if (size <= 1) {
+    step = 0.1;
+  } else if (size <= 5) {
+    step = 0.5;
+  } else if (size < 30) {
+    step = 1;
+  } else {
+    step = 2;
   }
-  else if(size<=5){
-    step=0.5;
-  }
-  else if(size<30){
-    step=1;
-  }
-  else{
-    step=2;
-  }
-  if(e.deltaY>0){
+  if (e.deltaY > 0) {
     //scrolling down
-    if(size-1 > 0){
-      size=size-step;
-      cursor_circle.setAttribute("r",size);
-      
+    if (size - 1 > 0) {
+      size = size - step;
+      cursor_circle.setAttribute("r", size);
 
-      text.style.fontSize=(size+5).toString()+"px";
-      
-      ctx.lineWidth=size*2;
-      self.size=size;
-      send({command:"broadcast",type:"ChS",size:size,id:userID});
+      text.style.fontSize = (size + 5).toString() + "px";
+
+      ctx.lineWidth = size * 2;
+      self.size = size;
+      send({ command: "broadcast", type: "ChS", size: size, id: userID });
     }
-  }
-  else{
+  } else {
     //scrolling up
-    if(size <101 ){
-      size=size+step;
-      cursor_circle.setAttribute("r",size);
-      
-      text.style.fontSize=(size+5).toString()+"px";
-      
-      ctx.lineWidth=size*2;
-      self.size=size;
-      send({command:"broadcast",type:"ChS",size:size,id:userID});
+    if (size < 101) {
+      size = size + step;
+      cursor_circle.setAttribute("r", size);
+
+      text.style.fontSize = (size + 5).toString() + "px";
+
+      ctx.lineWidth = size * 2;
+      self.size = size;
+      send({ command: "broadcast", type: "ChS", size: size, id: userID });
     }
   }
 });
 
-document.addEventListener("keydown", function(e){
-  send({command:"broadcast",type:"kp",key:e.key,id:self.id});
+document.addEventListener("keydown", function (e) {
+  send({ command: "broadcast", type: "kp", key: e.key, id: self.id });
   var user = getUser(self.id);
-  if(self.tool=="text"){
-    
+  if (self.tool == "text") {
     var input = $(".textInput.self")[0];
-    
-    if(e.key.length==1){
-      input.innerHTML=input.innerHTML+e.key;
-      user.text=user.text+e.key;
+
+    if (e.key.length == 1) {
+      input.innerHTML = input.innerHTML + e.key;
+      user.text = user.text + e.key;
     }
-    
-    switch(e.key){
+
+    switch (e.key) {
       case "Enter":
-        input.innerHTML="";
-        user.text="";
-        break
+        input.innerHTML = "";
+        user.text = "";
+        break;
       case "Backspace":
-        if(input.innerHTML){
-          input.innerHTML = input.innerHTML.slice(0,-1);
-          user.text = user.text.slice(0,-1);
+        if (input.innerHTML) {
+          input.innerHTML = input.innerHTML.slice(0, -1);
+          user.text = user.text.slice(0, -1);
         }
     }
-    
   }
 });
 
-
-function drawLine(pos,lastpos,user){
-    ctx.lineWidth=user.size*2;
-    //ctx.translate(0.5, 0.5);
-    ctx.beginPath();
-    ctx.moveTo(lastpos.x+100,lastpos.y+100);
-    ctx.lineTo(pos.x+100,pos.y+100);
-    ctx.stroke();
-    current_line.push(pos);
-    user.lastx=pos.x;
-    user.lasty=pos.y
+function drawLine(pos, lastpos, user) {
+  ctx.lineWidth = user.size * 2;
+  //ctx.translate(0.5, 0.5);
+  ctx.beginPath();
+  ctx.moveTo(lastpos.x + 100, lastpos.y + 100);
+  ctx.lineTo(pos.x + 100, pos.y + 100);
+  ctx.stroke();
+  current_line.push(pos);
+  user.lastx = pos.x;
+  user.lasty = pos.y;
 }
 
-function drawText(user){
-
-  var size = (user.size+5).toString();
-  ctx.fillStyle="#000";
-  ctx.font = size+"px sans-serif"
-  ctx.fillText(user.text,user.x+105,user.y+92+user.size+5)
-
+function drawText(user) {
+  var size = (user.size + 5).toString();
+  ctx.fillStyle = "#000";
+  ctx.font = size + "px sans-serif";
+  ctx.fillText(user.text, user.x + 105, user.y + 92 + user.size + 5);
 }
 
-function updateText(key,user){
-  console.log(user)
-  var input = $("."+user.id.toString()+" .textInput")[0];
-  if(key.length==1){
-    input.innerHTML=input.innerHTML+key;
-    user.text=user.text+key;
+function updateText(key, user) {
+  console.log(user);
+  var input = $("." + user.id.toString() + " .textInput")[0];
+  if (key.length == 1) {
+    input.innerHTML = input.innerHTML + key;
+    user.text = user.text + key;
   }
-  switch(key){
-      
+  switch (key) {
     case "Enter":
-      input.innerHTML="";
-      user.text="";
-      break
-      
+      input.innerHTML = "";
+      user.text = "";
+      break;
+
     case "Backspace":
-      if(input.innerHTML){
-        var newtext = input.innerHTML.slice(0,-1);
+      if (input.innerHTML) {
+        var newtext = input.innerHTML.slice(0, -1);
         input.innerHTML = newtext;
         user.text = newtext;
       }
-      break
-      
-      
+      break;
   }
 }
 
-function moveCursor(data){
-  
-  var id=data.id.toString();
+function moveCursor(data) {
+  var id = data.id.toString();
   var x = data.x;
   var y = data.y;
   var cursor = document.getElementsByClassName(id)[0];
-  cursor.style.left=x.toString()+'px';
-  cursor.style.top=y.toString()+'px';
+  cursor.style.left = x.toString() + "px";
+  cursor.style.top = y.toString() + "px";
 }
 
-function updateUser(user,data,fields){
-
-    for(var i=0;i<fields.length;i++){
-      var val = fields[i];
-      user[val] = data[val];
-    }
-  
+function updateUser(user, data, fields) {
+  for (var i = 0; i < fields.length; i++) {
+    var val = fields[i];
+    user[val] = data[val];
+  }
 }
-
 
 var clearBtn = $("#clearBtn")[0];
-clearBtn.addEventListener("click",function(){
+clearBtn.addEventListener("click", function () {
   clearBoard();
-  send({command:"broadcast",type:"clear",id:userID});
+  send({ command: "broadcast", type: "clear", id: userID });
 });
 
 var brushBtn = $("#brushBtn")[0];
 var textBtn = $("#textBtn")[0];
 
-
-
-brushBtn.addEventListener("click", function(){
+brushBtn.addEventListener("click", function () {
   var user = getUser(userID);
   var index = users.indexOf(user);
-  users[index].tool="brush"
-  send({command:"broadcast",type:"ChT",tool:"brush",id:self.id});
-  $(".text.self")[0].style.display="none";
-  $(".circle.self")[0].style.display="block";
+  users[index].tool = "brush";
+  send({ command: "broadcast", type: "ChT", tool: "brush", id: self.id });
+  $(".text.self")[0].style.display = "none";
+  $(".circle.self")[0].style.display = "block";
 });
 
-
-textBtn.addEventListener("click",function(){
+textBtn.addEventListener("click", function () {
   var user = getUser(userID);
   var index = users.indexOf(user);
-  users[index].tool="text";
-  send({command:"broadcast",type:"ChT",tool:"text",id:self.id});
-  $(".text.self")[0].style.display="block";
-  $(".circle.self")[0].style.display="none";
+  users[index].tool = "text";
+  send({ command: "broadcast", type: "ChT", tool: "text", id: self.id });
+  $(".text.self")[0].style.display = "block";
+  $(".circle.self")[0].style.display = "none";
 });
 
-
-function clearBoard(){
+function clearBoard() {
   console.log("clearing board");
-  ctx.fillStyle="#FFF";
-  ctx.fillRect(0,0,400,400)
+  ctx.fillStyle = "#FFF";
+  ctx.fillRect(0, 0, 400, 400);
 }
 
-function drawUser(data,id){
+function drawUser(data, id) {
   //draw each user with its current data from the server
   console.log("drawing user: ");
   console.log(data);
-  var cursor = $('<div></div>')[0];
-  cursor.setAttribute("class","cursor "+id.toString());
-  cursor.style.left=(data.x.toString()+"px");
-  cursor.style.top=(data.y.toString()+"px");
-  
-  var svg = document.createElementNS('http://www.ww3.org/2000/svg', 'svg');
-  svg.setAttribute("height","202px");
-  svg.setAttribute("width","202px");
-  var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-  circle.setAttribute("class",id.toString());
-  circle.setAttribute("stroke","grey");
-  circle.setAttribute("stroke-width","1");
-  circle.setAttribute("fill","none");
-  circle.setAttribute("cx","100");
-  circle.setAttribute("cy","100");
-  circle.setAttribute("r","10");
-  circle.setAttribute("height","auto");
+  var cursor = $("<div></div>")[0];
+  cursor.setAttribute("class", "cursor " + id.toString());
+  cursor.style.left = data.x.toString() + "px";
+  cursor.style.top = data.y.toString() + "px";
+
+  var svg = document.createElementNS("http://www.ww3.org/2000/svg", "svg");
+  svg.setAttribute("height", "202px");
+  svg.setAttribute("width", "202px");
+  var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+  circle.setAttribute("class", id.toString());
+  circle.setAttribute("stroke", "grey");
+  circle.setAttribute("stroke-width", "1");
+  circle.setAttribute("fill", "none");
+  circle.setAttribute("cx", "100");
+  circle.setAttribute("cy", "100");
+  circle.setAttribute("r", "10");
+  circle.setAttribute("height", "auto");
   var cursors = $(".cursors")[0];
-  var name = $("<text>"+id.toString()+"</text>")[0];
-  name.setAttribute("class","name "+id.toString());
+  var name = $("<text>" + id.toString() + "</text>")[0];
+  name.setAttribute("class", "name " + id.toString());
   var text = $("<text></text>")[0];
-  text.setAttribute("class","text "+id.toString());
-  text.style.width="400px";
-  text.style.fontSize=(data.size+5).toString()+"px";
+  text.setAttribute("class", "text " + id.toString());
+  text.style.width = "400px";
+  text.style.fontSize = (data.size + 5).toString() + "px";
   var line = $("<text>|</text>")[0];
   var textinput = $("<text></text>")[0];
-  textinput.setAttribute("class","textInput "+id.toString());
-  textinput.innerHTML=data.text;
+  textinput.setAttribute("class", "textInput " + id.toString());
+  textinput.innerHTML = data.text;
   text.appendChild(textinput);
   text.appendChild(line);
   svg.appendChild(circle);
   cursor.appendChild(svg);
   cursor.appendChild(name);
   cursor.append(text);
-  
+
   cursors.appendChild(cursor);
-  
 }
--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 // This will serve the static files in the /public folder on our server
 app.use(express.static("public"));
 
-const server = app.listen(process.env.PORT, function() {
+const server = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + server.address().port);
 });
-
 
 // Websocket Server:
 // We are using the external library 'ws' to set up the websockets on the server
@@ -20,12 +19,12 @@ var WebSocket = require("ws");
 var wsServer = new WebSocket.Server({ server });
 
 //keep track of the current users
-var current_users = []; 
+var current_users = [];
 
 // This function will send a message to all clients connected to the websocket:
 function broadcast(data) {
   updateUser(data);
-  wsServer.clients.forEach(client => {
+  wsServer.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN && data.id != client.id) {
       client.send(JSON.stringify(data));
     }
@@ -34,100 +33,92 @@ function broadcast(data) {
 
 // This outer function will run each time the Websocket
 // server connects to a new client:
-wsServer.on("connection", ws => {
+wsServer.on("connection", (ws) => {
   // We will store the id for this connection in the id property.
   ws.id = "";
   // This function will run every time the server recieves a message with that client.
-  ws.on("message", data => {
+  ws.on("message", (data) => {
     // Broadcast the received message back to all clients.
     data = JSON.parse(data);
-    
-    switch(data.command){
-        
-      case 'connect':
+
+    switch (data.command) {
+      case "connect":
         console.log("Message Received: ", data);
         ws.id = data.id;
         console.log("from connection Id:", ws.id);
-        
+
         var user = data;
-        delete user.command
+        delete user.command;
         current_users.push(user);
-        broadcast({command:"currentUsers",users:current_users});
+        broadcast({ command: "currentUsers", users: current_users });
         //save user to list of current users in room
         //when somebody joins, send them this list of users
-        
-        break
-      case 'broadcast':
-        broadcast(data);
-        break
-        
-    }
 
-    
+        break;
+      case "broadcast":
+        broadcast(data);
+        break;
+    }
   });
 
   ws.on("close", () => {
     console.log("Disconnected:", ws.id);
-    broadcast({command:"userLeft",id:ws.id});
-    current_users = current_users.filter(userdata =>{
+    broadcast({ command: "userLeft", id: ws.id });
+    current_users = current_users.filter((userdata) => {
       return userdata.id != ws.id;
     });
-    console.log("current users: "+current_users);
-    
-    
+    console.log("current users: " + current_users);
   });
 });
 
-function getUser(id){
-  var user = current_users.filter( a =>{
-    return a.id==id;
+function getUser(id) {
+  var user = current_users.filter((a) => {
+    return a.id == id;
   })[0];
-  return user
+  return user;
 }
 
-function updateUser(data){
+function updateUser(data) {
   var user = getUser(data.id);
-  if(user){
+  if (user) {
     var userdata = user.userdata;
-  
-    switch(data.type){
-      case 'Mm':
-        userdata.x=data.x;
-        userdata.y=data.y;
-        userdata.lastx=data.lastx;
-        userdata.lasty=data.lasty;
-        break
-      case 'Md':
-        userdata.mousedown=true;
-        break
-      case 'Mu':
-        userdata.mousedown=false;
-        break
-      case 'ChS':
-        userdata.size=data.size;
-        break
-      case 'ChT':
-        userdata.tool=data.tool;
-        break
-      case 'kp':
-        var key=data.key;
-        if(key.length==1){
-          userdata.text=userdata.text+key;
-        }
-        switch(key){
 
+    switch (data.type) {
+      case "Mm":
+        userdata.x = data.x;
+        userdata.y = data.y;
+        userdata.lastx = data.lastx;
+        userdata.lasty = data.lasty;
+        break;
+      case "Md":
+        userdata.mousedown = true;
+        break;
+      case "Mu":
+        userdata.mousedown = false;
+        break;
+      case "ChS":
+        userdata.size = data.size;
+        break;
+      case "ChT":
+        userdata.tool = data.tool;
+        break;
+      case "kp":
+        var key = data.key;
+        if (key.length == 1) {
+          userdata.text = userdata.text + key;
+        }
+        switch (key) {
           case "Enter":
-            userdata.text="";
-            break
+            userdata.text = "";
+            break;
 
           case "Backspace":
-            if(userdata.text){
-              userdata.text = userdata.text.slice(0,-1);
+            if (userdata.text) {
+              userdata.text = userdata.text.slice(0, -1);
             }
-            break
-
+            break;
         }
-        break
+        break;
     }
   }
 }
