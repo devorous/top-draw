fc204c1a11076d96166debf025a21f2d2774a02f
Glitch (multiplayer-websockets-example) <none>
2022-12-11 18:32:37 +0000

    🍩👘 Checkpoint ./public/index.html:3497662/171 ./public/client.js:3497662/142 ./public/css/style.css:3497662/129

--- public/client.js
+++ public/client.js
@@ -4,6 +4,7 @@ var users = [];
 var userID = Math.floor(Math.random() * 999999);
 
 var board = $("#board")[0];
+var topBoard = $("#topBoard")[0];
 var cursor = $(".cursor.self")[0];
 var cursor_circle = cursor.children[0].children[0];
 var text = $(".name.self")[0];
@@ -14,6 +15,7 @@ var size = 10;
 
 var ctx = board.getContext("2d");
 ctx.imageSmoothingQuality = "high";
+var ctx2 = topBoard.getContext("2d");
 
 var current_line = [];
 
@@ -36,9 +38,6 @@ var self = {
 
 // Add self player to beginning of players array:
 users.push(self);
-
-
-
 
 
 
@@ -232,6 +231,7 @@ board.addEventListener("mousedown", function (e) {
     ctx.lineWidth = user.size * 2;
     ctx.moveTo(e.layerX, e.layerY);
     ctx.lineTo(e.layerX, e.layerY);
+    ctx2.fillStyle=user.co
   }
 
   if (user.tool == "text" && user.text != "") {
