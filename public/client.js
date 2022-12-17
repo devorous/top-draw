9e3a292e558e4e13c5ca284bffb163f6194a6c3c
Glitch (multiplayer-websockets-example) <none>
2022-12-17 05:33:32 +0000

    🍿💶 Checkpoint ./public/client.js:3497662/1982 ./public/js/loadGimp.js:3497662/1398 ./public/index.html:3497662/67

--- public/client.js
+++ public/client.js
@@ -43,6 +43,8 @@ ctx.imageSmoothingQuality = "high";
 ctx2.imageSmoothingQuality = "high";
 ctx.lineCap = "round";
 
+var gimpData = null;
+
 var current_line = [];
 
 var connected=false;
@@ -708,10 +710,69 @@ window.addEventListener("resize", (e) => {
 })
   
 
-var file = null;
+function parseGihFile(data) {
+  // Use a DataView to read the binary data in the file
+  var view = new DataView(data);
+
+  // Extract the metadata from the file
+  var magicNumber = view.getUint32(0); // Should be "GIMP"
+  var numFrames = view.getUint32(4);
+  console.log("number of frames: ",numFrames)
+  var width = view.getUint32(8);
+  var height = view.getUint32(12);
+
+  // Extract the frame data and create image objects for each frame
+  var frames = [];
+  for (var i = 0; i < numFrames; i++) {
+    var offset = view.getUint32(16 + i * 4);
+    var frameData = data.slice(offset);
+    var image = new Image();
+    image.src = 'data:image/x-gimp-brush;base64,' + btoa(String.fromCharCode.apply(null, frameData));
+    frames.push(image);
+  }
+
+  return frames;
+}
+
+function parseGbrFile(data) {
+  // Use a DataView to read the binary data in the file
+  var view = new DataView(data);
+
+  // Extract the magic number from the file
+  var magicNumber = view.getUint32(0); // Should be "GIMP"
+
+  // Extract the metadata from the file
+  var width = view.getUint32(4);
+  var height = view.getUint32(8);
+  var colorDepth = view.getUint16(12);
+  var numColors = view.getUint16(14);
+  var offset = view.getUint32(16);
+
+  // Extract the brush data and create an image object
+  var brushData = data.slice(offset);
+  var image = new Image();
+  image.src = 'data:image/x-gimp-brush;base64,' + btoa(String.fromCharCode.apply(null, brushData));
+
+  return image;
+}
+
+
+
+var gimpFrames = [];
+
 document.getElementById('gimp-file-input').addEventListener('change', function(event) {
   // File selected by the user
-   file = event.target.files[0];
+   var file = event.target.files[0];
+    console.log(file.split(".")[1]);
+    var fileReader = new FileReader();
+  fileReader.onload = function() {
+    // File contents are stored in fileReader.result
+    var data = fileReader.result;
+    // You can now parse the .gih file using the parseGihFile() function from the previous example
+    var frames = parseGihFile(data);
+    gimpFrames=frames;
+  };
+  fileReader.readAsArrayBuffer(file);
 });
 
 
