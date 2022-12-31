bfdb0d376f5340a19c9c95281006fbaa6c5500cd
Glitch (top-draw) <none>
2022-12-31 18:43:28 +0000

    🚁🍥 Checkpoint ./public/client.js:3497662/9065 ./public/js/parseGimp.js:3497662/4827 ./public/js/drawingFunctions.js:3497662/4196 ./public/index.html:3497662/36

---
+++ public/js/drawingFunctions.js
@@ -0,0 +1,143 @@
+
+function calcCatmullRomCurve(points, tension) {
+  // Initialize the curve points array
+  const curvePoints = [];
+
+  // Add the first point to the curve points array
+  curvePoints.push(points[0]);
+
+  // Iterate over the points and generate the curve points
+  for (let i = 1; i < points.length - 2; i++) {
+    for (let t = 0; t < 1; t += 0.1) {
+      const t1 = t;
+      const t2 = t1 * t1;
+      const t3 = t2 * t1;
+
+      // Calculate the x and y values for the curve point
+      const x =
+        ((2 * points[i].x) +
+          (-points[i - 1].x + points[i + 1].x) * t1 +
+          (2 * points[i - 1].x - 5 * points[i].x + 4 * points[i + 1].x - points[i + 2].x) * t2 +
+          (-points[i - 1].x + 3 * points[i].x - 3 * points[i + 1].x + points[i + 2].x) * t3) *
+        tension;
+      const y =
+        ((2 * points[i].y) +
+          (-points[i - 1].y + points[i + 1].y) * t1 +
+          (2 * points[i - 1].y - 5 * points[i].y + 4 * points[i + 1].y - points[i + 2].y) * t2 +
+          (-points[i - 1].y + 3 * points[i].y - 3 * points[i + 1].y + points[i + 2].y) * t3) *
+        tension;
+
+      // Add the curve point to the array
+      curvePoints.push({ x, y });
+    }
+  }
+
+  // Add the last point to the curve points array
+  curvePoints.push(points[points.length - 1]);
+
+  return curvePoints;
+}
+
+function calcNaturalCubicSpline(points, tension) {
+  // Calculate the number of points
+  const numPoints = points.length;
+
+  // Initialize the coefficients array
+  const coefficients = [];
+
+  // Special case for the first point
+  coefficients[0] = {
+    a: 0,
+    b: 2,
+    c: 1,
+    d: (points[1].y - points[0].y) / 2,
+  };
+
+  // Special case for the last point
+  coefficients[numPoints - 1] = {
+    a: (points[numPoints - 1].y - points[numPoints - 2].y) / 2,
+    b: 2,
+    c: 1,
+    d: 0,
+  };
+
+  // Iterate over the remaining points
+  for (let i = 1; i < numPoints - 1; i++) {
+    const prevPoint = points[i - 1];
+    const currPoint = points[i];
+    const nextPoint = points[i + 1];
+
+    coefficients[i] = {
+      a: (nextPoint.y - currPoint.y) / 2 - (currPoint.y - prevPoint.y) / 2,
+      b: 2 * currPoint.y - 2 * nextPoint.y + (nextPoint.y - prevPoint.y) / tension,
+      c: (nextPoint.y - prevPoint.y) / tension - 2 * currPoint.y + 2 * prevPoint.y,
+      d: currPoint.y,
+    };
+  }
+
+  return coefficients;
+}
+
+// Define a function to perform spline interpolation
+function splineInterpolation(points, tension, numOfSegments) {
+  // Calculate the coefficients for the spline curve
+  const coefficients = calcNaturalCubicSpline(points, tension);
+
+  // Initialize the interpolated points array
+  const interpolatedPoints = [];
+
+  // Generate the interpolated points
+  for (let i = 0; i < points.length - 1; i++) {
+    for (let t = 0; t < numOfSegments; t++) {
+      const t1 = t / numOfSegments;
+      const t2 = t1 * t1;
+      const t3 = t2 * t1;
+
+      // Calculate the x and y values for the interpolated point
+      const x =
+        (2 * t3 - 3 * t2 + 1) * points[i].x +
+        (-2 * t3 + 3 * t2) * points[i + 1].x +
+        (t3 - 2 * t2 + t1) * coefficients[i].a +
+        (t3 - t2) * coefficients[i].b;
+      const y =
+        (2 * t3 - 3 * t2 + 1) * points[i].y +
+        (-2 * t3 + 3 * t2) * points[i + 1].y +
+        (t3 - 2 * t2 + t1) * coefficients[i].c +
+        (t3 - t2) * coefficients[i].d;
+
+      // Add the interpolated point to the array
+      interpolatedPoints.push({ x, y });
+    }
+  }
+
+  // Add the last point to the array
+  interpolatedPoints.push(points[points.length - 1]);
+
+  return interpolatedPoints;
+}
+
+
+// Define a function to draw a smooth curve through an array of points
+function drawSmoothCurve(ctx, points, tension, numOfSegments) {
+  // Interpolate the points using spline interpolation
+  const interpolatedPoints = splineInterpolation(points, tension, numOfSegments);
+
+// Set the line width and stroke style
+ctx.lineWidth = 3;
+ctx.strokeStyle = 'red';
+// Begin a new path
+ctx.beginPath();
+
+// Draw a curve through the interpolated points
+for (let i = 0; i < interpolatedPoints.length; i++) {
+const point = interpolatedPoints[i];
+if (i === 0) {
+ctx.moveTo(point.x, point.y);
+} else {
+ctx.lineTo(point.x, point.y);
+}
+}
+
+// Stroke the path
+ctx.stroke();
+}
---
+++ public/js/parseGimp.js
@@ -0,0 +1,192 @@
+//helper functions for parseGbr()
+function chunkToString(chunk){
+  var string="";
+  for(var i=0;i<chunk.length;i++){
+        var letter = String.fromCharCode(chunk[i.toString()]);
+        string+=letter;
+  }
+  return string
+}
+
+function concatChunk(chunk){
+  var hexString = ""
+  for(var i=0;i<chunk.length;i++){
+    var hex = chunk[i.toString()].toString(16);
+    if(hex.length==1){
+      hex="0"+hex;
+    }
+    hexString+=hex;
+  }
+  return hexString
+}
+
+
+function parseGbr(arrayBuffer){
+  
+  var view = new Uint8Array(arrayBuffer);
+  // Create an array to hold the chunks
+  var chunks = [];
+
+  // Iterate through the view and extract the chunks
+  var headerChunk = view.slice(0,4)
+  var headerLength = Number("0x"+concatChunk(headerChunk));
+  var chunks = [];
+  for(var i=0;i<=27;i=i+4){
+    var chunk = view.slice(i,i+4);
+    var chunkHex = concatChunk(chunk);
+    chunks.push(chunkHex);
+  }
+  var lastchunk = view.slice(28,headerLength-1);
+  var lastchunkHex = chunkToString(lastchunk);
+  chunks.push(lastchunkHex);
+  
+  //extract the values of the bytes in each chunk
+  var headerSize = Number("0x"+chunks[0]);
+  var version = Number("0x"+chunks[1]);
+  var width = Number("0x"+chunks[2]);
+  var height = Number("0x"+chunks[3]);
+  var colorDepth = Number("0x"+chunks[4]);
+  var magicNumber = chunks[5];
+  var spacing = Number("0x"+chunks[6]);
+  var brushName = chunks[7];
+  
+  var imageData = view.slice(headerLength,view.length);
+  
+  
+  //Create an object that contains all the information about the brush
+  var brushObject = {
+    headerSize: headerSize,
+    version: version,
+    width: width,
+    height: height,
+    colorDepth: colorDepth,
+    magicNumber: magicNumber,
+    spacing: spacing,
+    brushName: brushName
+  };
+
+  
+  
+  var gimpCanvas = document.createElement("canvas");
+  gimpCanvas.height=height;
+  gimpCanvas.width=width;
+  var gCtx = gimpCanvas.getContext("2d");
+  var gimpImageData = gCtx.createImageData(width, height);
+  const gData = gimpImageData.data;
+  
+  //if the image is RGBA
+  if(colorDepth==4){
+    for (let i = 0; i < gData.length; i += 4) {
+      var r = imageData[i];
+      var b = imageData[i+2];
+      var g = imageData[i+1];
+      var a = imageData[i+3];
+      gData[i] = r;    // Red value
+      gData[i + 1] = g;  // Blue value
+      gData[i + 2] = b;  // Green value
+      gData[i + 3] = a;  // Alpha value
+    }
+  }
+  
+  //if the image is greyscale
+  if(colorDepth==1){
+    for(let i= 0; i < gData.length; i+=1){
+      var v = imageData[i]; // greyscale value
+      gData[i*4] = 255-v;    
+      gData[i*4 + 1] = 255-v; 
+      gData[i*4 + 2] = 255-v;  
+      gData[i*4 + 3] = 255;  // Alpha value
+    }
+  }
+  
+  gCtx.putImageData(gimpImageData, 0, 0);
+  
+  var url = gimpCanvas.toDataURL('image/png', 1.0);
+  
+  brushObject.gimpUrl=url;
+  
+  return brushObject
+}
+
+
+//helper function for parseGih
+function splitUint8Array(array, delimiter) {
+  const chunks = [];
+  let chunk = [];
+  let i = 0;
+  for (const value of array) {
+    
+    if (value === delimiter) {
+      chunks.push(chunk);
+      chunk = [];
+      i+=1;
+      if(i==2){
+        break;
+      }
+    } else {
+      chunk.push(value);
+    }
+  }
+  if (chunk.length > 0) {
+    chunks.push(chunk);
+  }
+  return chunks;
+}
+
+
+function parseGih(arrayBuffer){
+  var view = new Uint8Array(arrayBuffer);
+  //Limit size of .gih brush bytes to avoid lag
+  if(view.length>1500000){
+    alert("Too big!!");
+  }
+  else{
+    //split off the two heading chunks by newline (10 in decimal)
+    var chunks = splitUint8Array(view, 10);
+    var name =  chunkToString(chunks[0]);
+    var info = chunkToString(chunks[1]);
+
+    var infoSplit = info.split(" ");
+    var gihObject = {};
+    for(var i=1; i<infoSplit.length; i++){
+      var splits = infoSplit[i].split(":");
+      var head = splits[0];
+      var value = splits[1];
+      if(Number(value)){
+        value = Number(value);
+      }
+      gihObject[head]=value;
+    }
+    
+    var data = view.slice(chunks[0].length+chunks[1].length+2);
+    var colorDepth = data[19];
+    var imageBytes = gihObject.cellheight*gihObject.cellwidth*colorDepth;
+    
+
+    var indices = [];
+    var cellSize = 0;
+    
+    //an accumulator to store the indices of each image location, for slicing
+    var acc = 0;
+    
+    for(var i=0;i<gihObject.ncells;i++){
+      indices.push(acc);
+      var headerChunk = data.slice(acc,acc+4);
+      var headerLength = Number("0x"+concatChunk(headerChunk));
+      var cellSize = imageBytes+headerLength;
+      acc+= cellSize;
+    }
+    indices.push(acc);
+    
+    var brushes = [];
+    
+    for(var i=0;i<gihObject.ncells;i++){
+      var index = indices[i];
+      var current_data = data.slice(index,index+indices[i+1]);
+      var current_brush = parseGbr(current_data) ;
+      brushes.push(current_brush);
+    }
+    gihObject.gBrushes=brushes;
+      return gihObject
+  }
+}
