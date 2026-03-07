8a6a8a64e00928d9eaa5d21452b48ce202b8c5de
Kyle D <devorous@hotmail.com>
2026-03-06 22:57:16 -0800

    Fixed brush icon previews next to usernames

--- src/ui/UI.js
+++ src/ui/UI.js
@@ -189,8 +189,8 @@ export class UI {
     this.icons = {
       select: this.createIcon('images/select-icon.svg'),
       brush: this.createIcon('images/brush-icon.svg'),
-      pen: this.createIcon('images/pen-icon.svg'),
-      flowPen: this.createIcon('images/pen-icon.svg'),
+      pen: this.createIcon('images/brush-icon.svg'),
+      flowPen: this.createIcon('images/brush-icon.svg'),
       ink: this.createIcon('images/brush-icon.svg'),
       line: this.createIcon('images/line-icon.svg'),
       rectangle: this.createIcon('images/rectangle-icon.svg'),
@@ -198,7 +198,10 @@ export class UI {
       text: this.createIcon('images/text-icon.svg'),
       erase: this.createIcon('images/eraser-icon.svg'),
       blur: this.createIcon('images/brush-icon.svg'),
-      circleBlur: this.createIcon('images/circle-icon.svg'),
+      circleBlur: this.createIcon('images/circle-blur-icon.svg'),
+      inkdropper: this.createIcon('images/inkdropper-icon.svg'),
+      pan: this.createIcon('images/move-icon.svg'),
+      rotate: this.createIcon('images/rotate-icon.svg'),
       imageBrush: this.createIcon('images/pepper.png')
     };
   }
