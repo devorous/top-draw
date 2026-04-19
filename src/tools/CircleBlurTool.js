a236655769e7080c21b5f913839d674dd43ecbd3
Kyle D <devorous@hotmail.com>
2026-04-18 17:20:14 -0700

    Revert "Batch circle blur compositing to prevent progressive color shifts"
    This reverts commit 1ccaeb719108535c6fd50e64399c9f9bcb211b03.

--- src/tools/CircleBlurTool.js
+++ src/tools/CircleBlurTool.js
@@ -192,8 +192,6 @@ export class CircleBlurTool extends Tool {
         this.lastStampPos.set(user.id, { x: pos.x, y: pos.y, radius });
       }
     }
-
-    this.board.requestUpdate();
   }
 
   /**
@@ -355,6 +353,8 @@ export class CircleBlurTool extends Tool {
     this.board.expandDirtyRect(user,
       Math.floor(x - radius - 2), Math.floor(y - radius - 2),
       Math.ceil(radius * 2 + 4), Math.ceil(radius * 2 + 4));
+
+    this.board.requestUpdate();
   }
 
   /**
@@ -406,6 +406,8 @@ export class CircleBlurTool extends Tool {
     this.board.expandDirtyRect(user,
       Math.floor(x - radius - 2), Math.floor(y - radius - 2),
       Math.ceil(radius * 2 + 4), Math.ceil(radius * 2 + 4));
+
+    this.board.requestUpdate();
   }
 
   /**
